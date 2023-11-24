f77c4b4f9aa6ec2604b38a4f1638ea4a977dccfd
ZainabAlfredji <103365087+ZainabAlfredji@users.noreply.github.com>
2023-11-24 12:11:48 +0100

    yes

--- Othello FX Edu/src/main/ExampleAgentA.java
+++ Othello FX Edu/src/main/ExampleAgentA.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.List;
+
 import com.eudycontreras.othello.capsules.AgentMove;
 import com.eudycontreras.othello.controllers.AgentController;
 import com.eudycontreras.othello.controllers.Agent;
@@ -48,12 +50,89 @@ public class ExampleAgentA extends Agent{
 	 * @return
 	 */
 	private AgentMove getExampleMove(GameBoardState gameState){
-		
+		/*int depth = 3; // Define your depth limit
+        AgentMove bestMove = null;
+        int bestScore = Integer.MIN_VALUE;
+        int alpha = Integer.MIN_VALUE;
+        int beta = Integer.MAX_VALUE;
+
+		// Use AgentController to generate possible moves
+        List<AgentMove> possibleMoves = AgentController.generateMoves(gameState, getPlayerTurn());
+
+        for (AgentMove move : possibleMoves) {
+            GameBoardState newState = gameState.applyMove(move);
+            int score = minimax(newState, depth - 1, alpha, beta, false);
+
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove = move;
+            }
+        }
+
+        return bestMove;*/
 		int waitTime = UserSettings.MIN_SEARCH_TIME; // 1.5 seconds
 		
 		ThreadManager.pause(TimeSpan.millis(waitTime)); // Pauses execution for the wait time to cause delay
 		
 		return AgentController.getExampleMove(gameState, playerTurn); // returns an example AI move Note: this is not AB Pruning
 	}
+
+	private int minimax(GameBoardState position, int depth, int alpha, int beta,  boolean maximizingPlayer){
+		if(depth == 0 || position.isTerminal()){
+			//return static evalutaion of position
+		}
+		if(maximizingPlayer){
+			//int maxEval = Integer.MIN_VALUE
+			//for each child of position
+			//int eval = minimax(child, depth - 1, alpha, beta, false)
+			//maxEval = Math.max(maxEval, eval)
+			//alpha = Math.max(alpha, eval)
+			//if(beta <= alpha){break}
+			//return maxEval
+		} else {
+			//int minEval = Integer.MAX_VALUE
+			//for each child of position
+			//int eval = minimax(child, depth - 1, alpha, beta, true)
+			//minEval = Math.min(maxEval, eval)
+			//beta = Math.min(beta, eval)
+			//if(beta <= alpha){break}
+			//return minEval
+		}
+	}
+
+	/*private int minimax(GameBoardState state, int depth, int alpha, int beta, boolean maximizingPlayer) {
+        if (depth == 0 || state.isTerminal()) {
+            return AgentController.evaluate(state, getPlayerTurn());
+        }
+
+        if (maximizingPlayer) {
+            int maxEval = Integer.MIN_VALUE;
+            for (AgentMove move : AgentController.generateMoves(state, getPlayerTurn())) {
+                GameBoardState newState = state.applyMove(move);
+                int eval = minimax(newState, depth - 1, alpha, beta, false);
+                maxEval = Math.max(maxEval, eval);
+                alpha = Math.max(alpha, eval);
+                if (beta <= alpha)
+                    break;
+            }
+            return maxEval;
+        } else {
+            int minEval = Integer.MAX_VALUE;
+            for (AgentMove move : AgentController.generateMoves(state, getOpponentTurn())) {
+                GameBoardState newState = state.applyMove(move);
+                int eval = minimax(newState, depth - 1, alpha, beta, true);
+                minEval = Math.min(minEval, eval);
+                beta = Math.min(beta, eval);
+                if (beta <= alpha)
+                    break;
+            }
+            return minEval;
+        }
+    }
+
+    private PlayerTurn getOpponentTurn() {
+        return getPlayerTurn() == PlayerTurn.PLAYER_ONE ? PlayerTurn.PLAYER_TWO : PlayerTurn.PLAYER_ONE;
+    }*/
+
 
 }
